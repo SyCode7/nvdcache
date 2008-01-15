ee59c3683158c258a7915ea2dc991dc081f3d99e
mcdett <mcdett@3b29e61f-b440-0410-a726-431280d8ad0e>
2008-01-15 01:23:41 +0000

    big updates

--- trunk/db_functions.php
+++ trunk/db_functions.php
@@ -38,13 +38,20 @@ function dbf_cache_stats($db_link) {
 	
 	$cache_stats =  mysqli_fetch_assoc($result);
 	
+	$cache_stats[seconds_since_last_update] = time() - $cache_stats[last_db_update_epoch];
+	$cache_stats[minutes_since_last_update] = $seconds_since_last_update/60;
+	$cache_stats[hours_since_last_update] = $minutes_since_last_update/60;
+	
 	return $cache_stats;
 }
 
-function dbf_put_cve_in_db($cve, $db_link) {
-	// if it exists teplace the current cve
-	$description_escaped = mysqli_real_escape_string($db_link, $cve[description]);
-	$query = "REPLACE INTO cve SET cve_name = '$cve[cve_name]', reject = '$cve[reject]', published_epoch = '$cve[published_epoch]', modified_epoch = '$cve[modified_epoch]', last_cache_update_epoch = '$cve[last_cache_update_epoch]', severity = '$cve[severity]', CVSS_score = '$cve[CVSS_score]', CVSS_vector = '$cve[CVSS_vector]', CVSS_version = '$cve[CVSS_version]', CVSS_base_score = '$cve[CVSS_base_score]', CVSS_impact_subscore = '$cve[CVSS_impact_subscore]', CVSS_exploit_subscore = '$cve[CVSS_exploit_subscore]', description = '$description_escaped'";
+function dbf_put_entry_in_db($entry, $db_link) {
+	// make sql safe the text
+	$entry_mysql_safe = mysqli_real_escape_string($db_link, $entry);
+	
+	$xml_entry = new SimpleXMLElement($entry);
+	
+	$query = "REPLACE INTO nvdEntry SET name = '$xml_entry[name]', type = '$xml_entry[type]', entry = '$entry_mysql_safe'";
 	
 	if(!$result = mysqli_query($db_link, $query)) {
 		$xml = c_initiate_xml($ini_array);
@@ -52,64 +59,6 @@ function dbf_put_cve_in_db($cve, $db_link) {
 		$xml_error->addchild('code', '500');
 		$xml_error->addchild('description', 'DB Error: '.mysqli_error($db_link));
 		c_announce($xml);
-	}
-	
-	// delete any exsisting refrences or software items
-	$query = "DELETE FROM cve_ref WHERE cve_name = '$cve[cve_name]'";
-	
-	if(!$result = mysqli_query($db_link, $query)) {
-		$xml = c_initiate_xml($ini_array);
-		$xml_error = $xml->addchild('error');
-		$xml_error->addchild('code', '500');
-		$xml_error->addchild('description', 'DB Error: '.mysqli_error($db_link));
-		c_announce($xml);
-	}
-	
-	// if there are refrences add them
-	if($cve[refs]) {
-		foreach ($cve[refs] as $key => $row) {
-			$row[ref_txt] = mysqli_real_escape_string($db_link, $row[ref_txt]);
-			$row[ref_source] = mysqli_real_escape_string($db_link, $row[ref_source]);
-			$row[ref_patch] = mysqli_real_escape_string($db_link, $row[ref_patch]);
-			$query = "INSERT INTO cve_ref (cve_name, ref_source, ref_url, ref_patch, ref_text) VALUES ('$cve[cve_name]', '$row[ref_source]', '$row[ref_url]', '$row[ref_patch]', '$row[ref_txt]')";
-			if(!$result = mysqli_query($db_link, $query)) {
-				$xml = c_initiate_xml($ini_array);
-				$xml_error = $xml->addchild('error');
-				$xml_error->addchild('code', '500');
-				$xml_error->addchild('description', 'DB Error: '.mysqli_error($db_link));
-				c_announce($xml);
-			}
-		}
-	}
-	
-	$query = "DELETE FROM cve_prod WHERE cve_name = '$cve[cve_name]'";
-	
-	if(!$result = mysqli_query($db_link, $query)) {
-		$xml = c_initiate_xml($ini_array);
-		$xml_error = $xml->addchild('error');
-		$xml_error->addchild('code', '500');
-		$xml_error->addchild('description', 'DB Error: '.mysqli_error($db_link));
-		c_announce($xml);
-	}
-	
-	// add any software refrences add those also
-	if($cve[cve_prod]) {
-		//echo "in prg\n";
-		foreach ($cve[cve_prod] as $key2 => $row2) {
-			//echo "in foreach\n";
-			$row2[prod_name] = mysqli_real_escape_string($db_link, $row2[prod_name]);
-			$row2[prod_vendor] = mysqli_real_escape_string($db_link, $row2[prod_vendor]);
-			$row2[prod_ver] = mysqli_real_escape_string($db_link, $row2[prod_ver]);
-			$query = "INSERT INTO cve_prod (cve_name, prod_name, prod_vendor, vers_num) VALUES ('$cve[cve_name]', '$row2[prod_name]', '$row2[prod_vendor]', '$row2[prod_ver]')";
-			
-			if(!$result = mysqli_query($db_link, $query)) {
-				$xml = c_initiate_xml($ini_array);
-				$xml_error = $xml->addchild('error');
-				$xml_error->addchild('code', '500');
-				$xml_error->addchild('description', 'DB Error: '.mysqli_error($db_link));
-				c_announce($xml);
-			}
-		}
 	}
 }
 
