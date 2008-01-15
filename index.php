cfe9ae9f0aaecb0b9605df5ba11ea307d3d9bc73
mcdett <mcdett@3b29e61f-b440-0410-a726-431280d8ad0e>
2008-01-15 01:23:41 +0000

    big updates

--- index.php
+++ index.php
@@ -13,50 +13,6 @@ PURPOSE.
 
 */
 
-require 'db_functions.php';
-require 'common_functions.php';
-
-$start_time_epoch = time();
-$this_programs_version = "0.1"; // Original release
-$this_programs_name = "cve";
-
-// load ini file into an array	
-if (file_exists("local_config.ini")) {
-	$ini_array = parse_ini_file("local_config.ini", true);
-} else {
-	$ini_array = parse_ini_file("config.ini", true); // load the main one
-	if(!$ini_array) {
-		$xml = c_initiate_xml($ini_array);
-		$xml_error = $xml->addchild('error');
-		$xml_error->addchild('code', '500');
-		$xml_error->addchild('description', 'Configuration files were not found.  Please review the http://code.google.com/p/nvdcache/ file.');
-		c_announce($xml);
-	}
-}
-
-$start_db_con_call = time();
-$db_link = dbf_connectDB($ini_array);
-$end_db_con_call = time();
-
-$seconds_to_make_con = $end_db_con_call - $start_db_con_call;
-
-$start_db_query_call = time();
-$cache_stats = dbf_cache_stats($db_link);
-$end_db_query_call = time();
-
-$seconds_to_make_query = $end_db_query_call - $start_db_query_call;
-
-$nvdCache_age_seconds = time() - $cache_stats[last_db_update_epoch];
-
-$msg = "Everything looks honky dory!";
-
-$xml = c_initiate_xml($ini_array);
-$xml_msg = $xml->addchild('status');
-$xml_msg->addchild('code', '200');
-$xml_msg->addchild('description', $msg);
-$xml_msg->addchild('cache_age_seconds', $nvdCache_age_seconds);
-$xml_msg->addchild('seconds_to_make_con', $seconds_to_make_con);
-$xml_msg->addchild('seconds_to_make_query', $seconds_to_make_query);
-c_announce($xml);
+echo "Hello!  You've reached a nvdCache.  Go see http://code.google.com/p/nvdcache/ for more information.";
 
 ?>
---
+++ updateCache.php
@@ -0,0 +1,179 @@
+<?php
+/*
+nvdCache
+Copyright (c) 2007 The Hursk Group, LLC. All rights reserved.
+
+www.hursk.com
+
+hurskgroup@hursk.com
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
+
+*/
+
+require 'db_functions.php';
+require 'common_functions.php';
+
+$start_time_epoch = time();
+$this_programs_version = "0.1"; // Original release
+$this_programs_name = "updateDataSource";
+
+// load ini file into an array	
+if (file_exists("local_config.ini")) {
+	$ini_array = parse_ini_file("local_config.ini", true);
+} else {
+	$ini_array = parse_ini_file("config.ini", true); // load the main one
+	if(!$ini_array) {
+		$xml = c_initiate_xml($ini_array);
+		$xml_error = $xml->addchild('error');
+		$xml_error->addchild('code', '500');
+		$xml_error->addchild('description', 'Configuration files were not found.  Please see http://code.google.com/p/nvdcache/ for information.');
+		c_announce($xml);
+	}
+}
+
+$db_link = dbf_connectDB($ini_array);
+
+// grab the cache stats from the table.  This will tell us the last time the db was updated (if at all)
+$cache_stats = dbf_cache_stats($db_link);
+
+if($cache_stats[last_db_update_epoch] == 1000000) { // new install
+	// get all data from nvd and load it up.
+	full_db_load($ini_array, $db_link);
+	dbf_update_stats($db_link);
+	$cache_stats = dbf_cache_stats($db_link);
+} elseif($cache_stats[hours_since_last_update] > $ini_array[nvdCache][update_freq_hours]) { // she hasn't been updated in the time frame
+	// go and get the modifed file from nvd and run it through the db
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_modified];
+	stream_load_xml($url, $db_link);
+	dbf_update_stats($db_link);
+	$msg .= 'Due for a modified update.';
+} else {
+	// nothing for now.
+}
+
+mysqli_close($db_link);
+
+$run_time = time() - $start_time_epoch;
+
+$msg .= ' Took '.$run_time.' seconds to complete';
+$nvdCache_age_seconds = time() - $cache_stats[last_db_update_epoch];
+
+$xml = c_initiate_xml($ini_array);
+$xml_msg = $xml->addchild('status');
+$xml_msg->addchild('code', '200');
+$xml_msg->addchild('description', $msg);
+$xml_msg->addchild('cache_age_seconds', $nvdCache_age_seconds);
+$xml_msg->addchild('oldest_update_item_epoch', $oldest_update_epoch);
+c_announce($xml);
+
+
+function full_db_load($ini_array, $db_link) {
+	
+	// 
+	// below has been implemented very poorly.  It must be re-written to just figure the currrent
+	// year and do a for each back to 2002.
+	//
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2002".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2003".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2004".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2005".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2006".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2007".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_year_pre]."2008".$ini_array[cve][url_cve_year_post];
+	stream_load_xml($url, $db_link);
+	
+	$url = $ini_array[cve][url_base].$ini_array[cve][url_cve_modified];
+	stream_load_xml($url, $db_link);
+}
+
+function stream_load_xml($url, $db_link) {
+	$start_delimiter = "<entry type=";
+	$end_delimiter = "</entry>";
+	$byte_chunk_size = 64;
+	
+	
+	$handle = fopen("$url", "r");
+	/*if(!$handle) {
+		$xml = c_initiate_xml($ini_array);
+		$xml_error = $xml->addchild('error');
+		$xml_error->addchild('code', '500');
+		$xml_error->addchild('description', 'Could not establish a read handle to '.$url);
+		c_announce($xml);
+	} */
+
+	$in_entry = 0;
+
+	while ($data = fread($handle, $byte_chunk_size)) {
+		$read_data .= $data; // append the streaming data unto the variable
+		
+		// use the start_delimiter to detect the begginging of an xml entry
+		$read_data_exploded_start = explode($start_delimiter, $read_data);
+		
+		// if the array has more than one item then we hit upon the entry.
+		if (count($read_data_exploded_start) > 1 && !$in_entry) {
+
+			$in_entry = 1;
+
+			// what was infront of the delimieter is poo
+			$poo = array_shift($read_data_exploded_start);
+			
+			// put the start delimieter back unto the string becasue it was removed
+			// with the delimeter 'explode' call.
+			$read_data = $start_delimiter;
+			
+			// making sure that all left delimited items get added back
+			// to the read string
+			foreach ($read_data_exploded_start as $value) {
+				$read_data .= $value;
+			}
+		}
+		
+		// look for the end delimiter
+		$read_data_exploded_end = explode($end_delimiter, $read_data);
+		
+		// look for the end delimieter to indicate that we've gotten to the end of the
+		// entry
+		if (count($read_data_exploded_end) > 1) {
+			$in_entry = 0;
+			
+			$poo = array_shift($read_data_exploded_end);
+			
+			// we finaly have a full entry as string
+			$xml_entry_as_string = $poo.$end_delimiter;
+			
+			// put it into the db.
+			dbf_put_entry_in_db($xml_entry_as_string, $db_link);
+			
+			//$xml = new SimpleXMLElement($xml_entry_as_string);
+			
+			//echo $xml->asXML();
+			
+			// reset the read_data variable
+			
+			$read_data = '';
+			
+			// set the read_data variable with the end of the exploded
+			// data as to allow it to make the next front end delimiter
+			// check
+			foreach ($read_data_exploded_end as $value) {
+				$read_data .= $value;
+			} 	
+		}
+	}
+}
+?>
